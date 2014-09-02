75f2179e87790a7fb1f31907972911a7e9e70349
kendell clark <coffeekingms@gmail.com>
2014-09-01 22:50:30 -0500

    more work on daisy.py

--- src/daisy.py
+++ src/daisy.py
@@ -5,20 +5,19 @@
 #import the xml parser
 import xml.dom.minidom as md
 #define the class for daisy 3 talking books
- class daisy3 :
-def __init__ (self, title, author, multimediaType) 
-    
-def getBookTitle(self):
+class daisy3 :
+        def __init__ (self, title, author, multimediaType):
+    def getBookTitle(self):
     """
     Returns the book's title
     """
     book_title = ''
     title = self.tree.getElementsByTagName('title')
     for i in range(len(title)):
-      if title[i].hasAttribute('name'):
-        if title[i].attributes['name'].value == 'dc:title':
-          book_title = title[i].attributes['content'].value
-        break
+    if title[i].hasAttribute('name'):
+    if title[i].attributes['name'].value == 'dc:title':
+    book_title = title[i].attributes['content'].value
+    break
     return book_title
 def getAuthor(self):
         #returns the author of the book
@@ -29,7 +28,7 @@ def getAuthor(self):
         if author[i].attributes['name'].value == 'dc:creator':
 break
 return bookAuthor
-def getMultimediaType(self)
+def getMultimediaType(self):
         #gets the type of daisy book we're looking at here, not the format.
         multimediaType=""
         type=self.tree.getElementsByTagname(type)
