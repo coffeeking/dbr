c5abd0eb92f13fb198d5d9c6f09fefb37e9cb80d
Storm Dragon <stormdragon2976@gmail.com>
2014-09-01 20:58:36 -0400

    fixed config.py bugs.

--- src/config.py
+++ src/config.py
@@ -7,20 +7,21 @@
 #import the xdg module
 import xdg, os
 print("we have xdg imported.")
+
 class config:
-        def __init__(self, folder):
+    def __init__(self, folder):
         #check for the presence
-#of xdg-user-home, and if so, respect it. If not, default to ~/.config
-                settingsDir=""
+        #of xdg-user-home, and if so, respect it. If not, default to ~/.config
+        settingsDir = ""
         #try getting the xdg-user-home variable. If it isn't set, default #to ~/.config
-        xdg-home=os.environ.get('XDG_CONFIG_HOME')
-if xdg-home=="":
-        print("we have xdg home.")
-        #apparently the xdg-home variable isn't set, default to ~/.config
-        settingsDir=os.path.join(_home, '.config'/dbr)
-else:
-        #get the path of xdg-home and set the settingsDir to that location
-        print("oops, no xdg home.")
-        settingsDir=os.environ.get('XDG_CONFIG_HOME'/dbr)
+        #xdg-home = os.environ.get('XDG_CONFIG_HOME')
+        if xdg-home == "":
+            print("we have xdg home.")
+            #apparently the xdg-home variable isn't set, default to ~/.config
+            settingsDir = os.path.join(_home, '.config'/dbr)
+        else:
+            #get the path of xdg-home and set the settingsDir to that location
+            print("oops, no xdg home.")
+            settingsDir = os.environ.get('XDG_CONFIG_HOME'/dbr)
         return config
 
