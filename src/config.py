6c6f484cc154c8ad33ad865d30d661b903ca6e7b
Storm Dragon <stormdragon2976@gmail.com>
2014-09-01 21:54:47 -0400

    Startedrewrite of config.py. Sample of path get method.

--- src/config.py
+++ src/config.py
@@ -6,22 +6,18 @@
 #describes and implements an xdg compliant config scheme for dbr, storing #your bookmarks, list of books, and preferences inside of a folder named #after dbr
 #import the xdg module
 import xdg, os
-print("we have xdg imported.")
 
 class config:
-    def __init__(self, folder):
+    def get_settings_directory(self):
         #check for the presence
         #of xdg-user-home, and if so, respect it. If not, default to ~/.config
-        settingsDir = ""
         #try getting the xdg-user-home variable. If it isn't set, default #to ~/.config
-        #xdg-home = os.environ.get('XDG_CONFIG_HOME')
-        if xdg-home == "":
-            print("we have xdg home.")
-            #apparently the xdg-home variable isn't set, default to ~/.config
-            settingsDir = os.path.join(_home, '.config'/dbr)
+        if os.environ.get('XDG_CONFIG_HOME') == "":
+            settingsDir = os.path.join(xdg_home, 'dbr')
         else:
-            #get the path of xdg-home and set the settingsDir to that location
-            print("oops, no xdg home.")
-            settingsDir = os.environ.get('XDG_CONFIG_HOME'/dbr)
-        return config
+            #get the path of ~/.config and set the settingsDir to that location
+            settingsDir = os.path.join(os.environ.get('HOME'), '.config/dbr')
+        return settingsDir
 
+folder = config()
+print folder.get_settings_directory()
